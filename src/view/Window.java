d577ceb2a6b3cbf0a69d43b4d0467c35dab69230
Dagbedji Fagnisse <dfagnisse@gmail.com>
2013-02-22 23:12:11 -0500

    renamed Canvas as Windows

---
+++ src/view/Window.java
@@ -0,0 +1,29 @@
+package view;
+
+import java.awt.MenuBar;
+import java.util.List;
+
+public class Window {
+
+  //TODO: Additional
+    private List<FileView> myFileViews;
+    private FileView activeTab;
+    private MenuBar myMenuBar;
+    
+    public void openFile() {
+        
+    }
+    
+    public void saveFile() {
+        
+    }
+
+    public void newFile() {
+    
+    }
+    
+    
+    public void quit() {
+        
+    }
+}
