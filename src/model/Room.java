b9403d19dac0e4a1f25dc036e69870786426bbd8
thomasvarner <thomas.varner@duke.edu>
2013-02-26 22:21:20 -0500

    updated room and pentrail

--- src/model/Room.java
+++ src/model/Room.java
@@ -8,6 +8,7 @@ public class Room extends Observable {
     private int myID;
     private Turtle myTurtle;
     
+    
     public Room (int id) {
         myID = id;
     }
@@ -19,4 +20,6 @@ public class Room extends Observable {
     public void paint(Graphics2D pen) {
         
     }
+    
+    public void 
 }
--- src/model/Turtle.java
+++ src/model/Turtle.java
@@ -4,12 +4,33 @@ import java.awt.Dimension;
 import util.Location;
 import util.Pixmap;
 import util.Sprite;
+import java.util.Observable; 
+import java.util.Observer; 
 
-public class Turtle extends Sprite {
+public class Turtle extends Sprite extends Observable {
 
+    private Location myLocation; 
+    private Location myHead; 
+    
     public Turtle (Pixmap image, Location center, Dimension size) {
         super(image, center, size);
         // TODO Auto-generated constructor stub
     }
 
+    public void update() { 
+        
+    }
+    
+    public void move() { 
+        
+    }
+    
+    public void rotate() { 
+        
+    }
+    
+    public void checkBoundaries () { 
+        
+    }
+    
 }
