b40b9c330497502274ba44746c4b49547835fa03
Dagbedji Fagnisse <dfagnisse@gmail.com>
2013-02-26 22:28:00 -0500

    fixing a few conflicts

--- src/view/MenuBarView.java
+++ src/view/MenuBarView.java
@@ -30,19 +30,36 @@ public class MenuBarView extends JMenuBar {
     public JMenu makeFileMenu() {
         JMenu result = new JMenu(Window.myResources.getString("FileMenu"));
         result.add(Window.myResources.getString("SaveCommand"));
-        result.add(new AbstractAction(Window.myResources.getString("NewCommand")) {
-            @Override
-            public void actionPerformed (ActionEvent e) {
-                int response = myChooser.showOpenDialog(null);
-                if (response == JFileChooser.APPROVE_OPTION) {
-                    myWindow.addTab();
-                }
-            }
-        });
-        result.add(Window.myResources.getString("OpenCommand"));
+        result.add(new NewTabAction());
+        result.add(new OpenFileAction());
         result.add(new JSeparator());
         result.add(Window.myResources.getString("QuitCommand"));
         return result;
+    }
+    
+    class NewTabAction extends AbstractAction {
+        public NewTabAction() {
+            super(Window.myResources.getString("NewCommand"));
+        }
+        @Override
+        public void actionPerformed (ActionEvent e) {
+            myWindow.addTab();
+        } 
+    }
+    
+    class OpenFileAction extends AbstractAction {
+
+        public OpenFileAction() {
+            super(Window.myResources.getString("OpenCommand"));
+        }
+        @Override
+        public void actionPerformed (ActionEvent e) {
+            int response = myChooser.showOpenDialog(null);
+            if (response == JFileChooser.APPROVE_OPTION) {
+//                myWindow.addTab(response);
+            }
+        }
+        
     }
 
 }
