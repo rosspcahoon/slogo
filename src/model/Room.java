c154092a7843538d26f8726ba58c3a41a0123563
thomasvarner <thomas.varner@duke.edu>
2013-02-27 01:49:01 -0500

    made updates to room, turtle, and a small revision to sprite

--- src/model/Room.java
+++ src/model/Room.java
@@ -2,34 +2,69 @@ package model;
 
 import java.awt.Graphics2D;
 import java.util.Observable;
+import util.Location;
+import util.Pixmap; 
 
 public class Room extends Observable implements Renderable{
 
     private int myID;
     private Turtle myTurtle;
+    private boolean myTurtlePenStatus;
+    private double myTurtleHead; 
+    private Location myTurtleLocation;
+    
     private Status myStatus;
+    
+    
     
     
     public Room (int id) {
         myID = id;
         myTurtle = new Turtle();
+        myTurtlePenStatus = myTurtle.getPenStatus(); 
+        myTurtleLocation = myTurtle.getCenter(); 
+        myTurtleHead = myTurtle.getHead(); 
     }
-
+    
     public int getID () {
         return myID;
     }
     
-    @Override
+    public void penOnOff () { 
+        myTurtle.togglePen(); 
+        myTurtlePenStatus = myTurtle.getPenStatus(); 
+    }
+    
+    public boolean penStatus () { 
+        return myTurtlePenStatus; 
+    }
+    
+    public Location getTurtleLocation () { 
+        return myTurtleLocation; 
+    }
+    
+    public double getTurtleHead () { 
+        return myTurtleHead; 
+    }
+    
+    public void rotateTurtle(Graphics2D pen, double angle) { 
+        
+        myTurtle.rotate(pen, angle); 
+    }
+    
+    public void moveTurtleForward(double distance) { 
+        myTurtle.move(distance); 
+    }
+    
     public Status getState() {
         return myStatus;
     }
     
-    @Override
+    // draws line wherever turtle goes (if pen is on) 
     public void paint(Graphics2D pen) {
         myTurtle.paint(pen);
-        pen.drawLine((int) myTurtle.getOldX(), (int) myTurtle.getOldY(), (int) 
-                     myTurtle.getCurrentX(),(int) myTurtle.getCurrentY());
+        pen.drawLine((int) myTurtle.getOldLocation().getX(), (int) myTurtle.getOldLocation().getY(), (int) 
+                     myTurtle.getCurrentLocation().getX(),(int) myTurtle.getCurrentLocation().getY());
     }
     
-    public void 
 }
--- src/model/Turtle.java
+++ src/model/Turtle.java
@@ -4,37 +4,109 @@ import java.awt.Dimension;
 import util.Location;
 import util.Pixmap;
 import util.Sprite;
+import util.Vector;
 import java.util.Observable; 
 import java.util.Observer; 
+import java.awt.Graphics2D;
 
-public class Turtle extends Sprite {
-
-    private Location myLocation; 
-    private Location myHead; 
+public class Turtle extends Sprite  {
     
-    public Turtle (Pixmap image, Location center, Dimension size) {
-        super(image, center, size);
-        // TODO Auto-generated constructor stub
+    //turtle's head (angle) in degrees 
+    private double myHead; 
+    
+    private Location myOldLocation; 
+    private Location myCurrentLocation; 
+    
+    //turtle's pen 
+    private boolean myPenUp = false; 
+    
+    //frame default sizes (as of now) 
+    private static final int DEFAULT_FRAME_TOP = 250; 
+    private static final int DEFAULT_FRAME_BOTTOM = -250; 
+    private static final int DEFAULT_FRAME_RIGHT = 350; 
+    private static final int DEFAULT_FRAME_LEFT = -350; 
+    
+    // need turtle image
+    private static final Pixmap DEFAULT_TURTLE_IMAGE = new Pixmap("turtle.gif");
+    private static final Dimension DEFAULT_TURTLE_SIZE = new Dimension (20,20); 
+    private static Location initialLocation = new Location(0,0); 
+    private static double initialAngle = 90; 
+    
+    public Turtle () { 
+        super(DEFAULT_TURTLE_IMAGE, initialLocation, DEFAULT_TURTLE_SIZE); 
+        myOldLocation = null; 
+        myCurrentLocation = initialLocation; 
+        myHead = initialAngle; 
     }
-
-    public void update() { 
+ 
+    public void move (double distance) { 
+        super.translate(new Vector(myHead, distance)); 
+        setLocations();  
+    }
+    
+    public void setLocations () { 
+        myOldLocation = myCurrentLocation; 
+        myCurrentLocation = new Location (super.getCenter());
+    }
+    
+    public Location getOldLocation() { 
+        return myOldLocation; 
+    }
+    
+    public Location getCurrentLocation () { 
+        return myCurrentLocation; 
+    }
+    
+    public double getHead() { 
+        return myHead; 
+    }
+    
+    public boolean getPenStatus () { 
+        return myPenUp; 
+    }
+    
+    public void togglePen () { 
+        myPenUp = !(myPenUp); 
+    }
+    
+    // rotates turtle by specified angle (in degrees) 
+    public void rotate(Graphics2D pen, double angle) { 
+        myHead += angle; 
+        super.getView().paint(pen, super.getCenter(), DEFAULT_TURTLE_SIZE, angle); 
+    }
+    
+    public void wrapAround () { 
+        if (super.getBottom() > DEFAULT_FRAME_TOP) { 
+            super.setCenter(super.getX(), DEFAULT_FRAME_BOTTOM - (super.getHeight() / 2)); 
+        }
+        if (super.getTop() < DEFAULT_FRAME_BOTTOM) { 
+            super.setCenter(super.getX(), DEFAULT_FRAME_TOP + (super.getHeight() / 2)); 
+        }
+        if (super.getLeft() > DEFAULT_FRAME_RIGHT) { 
+            super.setCenter(DEFAULT_FRAME_LEFT - (super.getWidth() / 2), super.getY()); 
+        }
+        if (super.getRight() < DEFAULT_FRAME_LEFT) { 
+            super.setCenter(DEFAULT_FRAME_RIGHT + (super.getWidth() / 2), super.getY()); 
+        }
+    } 
+    
+    //TO-DO
+    public void notifyObservers (Object arg) { 
         
     }
     
-    public void move(String direction, int distance) { 
+    //TO-DO
+    public void addObserver (Object o) { 
         
     }
     
-    public void rotate(double angle) { 
+    //TO-DO
+    public void setChanged () { 
         
     }
     
-    public void wrapAround() { 
-        
+    //TO_DO
+    public boolean hasChanged () { 
+        return true; 
     }
-    
-    public void checkBoundaries () { 
-        
-    }
-    
 }
